f9ddd1dfde857600f5198ae0b54f295a3c4d7481
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-05-13 14:33:55 -0700

    Adds validation and happy sad paths to controllers

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  validates :title, presence: true, length: { in: 1..50 }
+  validates :year, numericality: { only_integer: true }
+  validates :plot, length: { in: 1..500 }
+  validates :director, length: { in: 2..50 }
+  validates :english, presence: true
 end
