ee1e5630db1ba29b7fc78e9795fe32ef73dd6d53
Katie Tensfeldt <kmtensfeldt@gmail.com>
2021-05-25 15:51:57 -0700

    Adds genres and join table for genre and movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ApplicationRecord
 
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
 
   validates :title, presence: true, length: { in: 1..50 }
   validates :year, numericality: { only_integer: true }
@@ -8,5 +10,9 @@ class Movie < ApplicationRecord
   validates :director, length: { in: 2..50 }
   validates :english, inclusion: [true, false]
   validates :english, exclusion: [nil]
+
+  def genre_names
+    genres.map{ |genre| genre.name }
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,20 @@
 # Actor.create(first_name: FFaker::Name::first_name, last_name: FFaker::Name::last_name, known_for: FFaker::Movie::title)
 # Actor.create(first_name: FFaker::Name::first_name, last_name: FFaker::Name::last_name, known_for: FFaker::Movie::title)
 
-Movie.create(title: "Tangled", year: 2010, plot: "The magically long-haired Rapunzel has spent her entire life in a tower, but now that a runaway thief has stumbled upon her, she is about to discover the world for the first time, and who she really is.")
-Movie.create(title: "The Lord of the Rings: The Return of the King", year: 2003, plot: "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.")
+# Movie.create(title: "Tangled", year: 2010, plot: "The magically long-haired Rapunzel has spent her entire life in a tower, but now that a runaway thief has stumbled upon her, she is about to discover the world for the first time, and who she really is.")
+# Movie.create(title: "The Lord of the Rings: The Return of the King", year: 2003, plot: "Gandalf and Aragorn lead the World of Men against Sauron's army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.")
 
+# Genre.create(name: "Adventure")
+# Genre.create(name: "Family")
+# Genre.create(name: "Comedy")
+# Genre.create(name: "Drama")
+
+MovieGenre.create(genre_id: 1, movie_id: 3)
+MovieGenre.create(genre_id: 4, movie_id: 3)
+MovieGenre.create(genre_id: 2, movie_id: 4)
+MovieGenre.create(genre_id: 3, movie_id: 4)
+MovieGenre.create(genre_id: 3, movie_id: 7)
+MovieGenre.create(genre_id: 2, movie_id: 7)
+MovieGenre.create(genre_id: 1, movie_id: 7)
+MovieGenre.create(genre_id: 3, movie_id: 8)
+MovieGenre.create(genre_id: 4, movie_id: 8)
